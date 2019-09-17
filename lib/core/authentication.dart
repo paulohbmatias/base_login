203827145b00c211c46088807c810d569623fe23
Paulo <paulohbmatias@cc.ci.ufpb.br>
2019-09-17 11:02:51 -0300

    added database nosql

--- lib/core/authentication.dart
+++ lib/core/authentication.dart
@@ -1,8 +1,9 @@
+import 'package:base_login/shared/models/token.dart';
 import 'package:base_login/shared/models/user.dart';
 
 class Authentication{
   
-  User signIn(String email, String password){
+  Future<Token> signIn(String email, String password){
       
   }
 }
